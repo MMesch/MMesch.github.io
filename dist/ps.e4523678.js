41e0f641cf14ac934892bc6d2d8422df56f22fde
Matthias Meschede <MMesch@users.noreply.github.com>
2021-10-19 19:38:49 +0200

    verified version

--- dist/ps.e4523678.js
+++ dist/ps.e4523678.js
@@ -51335,7 +51335,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37761" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34865" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
