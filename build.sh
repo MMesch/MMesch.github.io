bc8bdd9e8d5966c4c92d579a615878d4aaf7749c
Matthias Meschede <MMesch@users.noreply.github.com>
2022-05-09 10:55:45 +0200

    rename folders for better routing

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
 npx tailwindcss -i tailwind.css -o build/style.css
-for i in blog/*.md
+for i in docs/posts/*.md
   do pandoc $i --filter pandoc-katex-filter --highlight-style pygments -o ${i/.md/.html}
 done
